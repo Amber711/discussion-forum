f45ff6639a26f133e70489351591ba79658a3c59
amberoffduty <380914555@qq.com>
2017-05-17 04:56:33 +0800

    test

--- ed-client/src/Lecture/QuestionPage.js
+++ ed-client/src/Lecture/QuestionPage.js
@@ -9,7 +9,7 @@ class QuestionPage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {questionDetail: null}
+        this.state = {questionDetail: {}}
     }
 
     componentDidMount() {
@@ -35,7 +35,7 @@ class QuestionPage extends React.Component {
                 id: 0,
                 title: "Confused on constructing RectHV for given Node",
                 date: "2 months ago",
-                author: "Amber Lum",
+                author: "J.D. DeVaughn-Brown",
                 content: `I'm confused as to how to define the axis-aligned rectangle corresponding to the node. For each node inserted, it forms two rectangles (one above and one below, or one to the right and one to the left).
                         
                         Which rectangle am I suppose to be thinking of when constructing the rectangle corresponding to the node?
@@ -43,7 +43,7 @@ class QuestionPage extends React.Component {
                         Any help would be greatly appreciated.`,
                 answers: [
                     {
-                        username: 'Stephen Chan',
+                        username: 'christian cleber masdeval braz',
                         email: '380914555@qq.com',
                         date: '2 months ago',
                         upvote: 10,
@@ -93,13 +93,20 @@ class QuestionPage extends React.Component {
 
     render() {
         return (
+            /*question from author*/
             <div className="container q-detail-wrapper">
                 <div className="panel panel-default q-desc">
                     <ul className="panel-body row author-q-desc">
 
                         <li className="col-md-9 text-left">
-                            <p>What is a minimum viable product?</p>
-                            <span aria-hidden="true"> Lily Asked 3 hours ago</span>
+                            <h3 className="question-title">Nearest Neighbour search</h3>
+
+                            <p>I'm confused as to how to define the axis-aligned rectangle corresponding to the node. For each node inserted, it forms two rectangles (one above and one below, or one to the right and one to the left).
+
+                                Which rectangle am I suppose to be thinking of when constructing the rectangle corresponding to the node?
+
+                                Any help would be greatly appreciated.</p>
+                            <span aria-hidden="true" className="author-box"> {this.state.questionDetail.author}&nbsp;&nbsp;Asked&nbsp;&nbsp;{this.state.questionDetail.date}&nbsp;&nbsp;</span>
                         </li>
                         <li className="col-md-2 pull-right text-center">
                             <a href="#" className="click-to-follow">
@@ -116,23 +123,29 @@ class QuestionPage extends React.Component {
                     </ul>
                 </div>
 
+                     {/* filter bar */}
                 <ul className="nav nav-pills filter-qs" >
                     <li className="filter-items" role="presentation"><a href="#">Top</a></li>
                     <li className="filter-items" role="presentation"><a href="#">Recent</a></li>
                 </ul>
 
+
+                    {/*reply detail*/}
                 <ul className="panel panel-default reply-list">
                     <li className="panel-body row">
-                        <div className="col-md-1">
+
+                        <div className="col-md-1 reply-like">
                             <div className="agree pull-left text-center">
                                 <span className="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
                                 <p>26</p>
 
                             </div>
                         </div>
+
+
                         <div className="col-md-11">
                             <div className="reply-caption">
-                                <span className="reply-name">Christian Cleber</span>&nbsp;&nbsp;
+                                <span className="reply-name">Amber</span>&nbsp;&nbsp;
                                 <span className="reply-profile">Mentor</span>&nbsp;&nbsp;
                                 <span className="reply-date">a month ago</span>
                             </div>
@@ -159,6 +172,8 @@ class QuestionPage extends React.Component {
 
                             </div>
                         </div>
+
+                        {/*reply input box*/}
                         <div className="comment-input panel-body row">
                             <span className="profile-area">FM</span>
                             <div className="reply-box">
@@ -173,6 +188,83 @@ class QuestionPage extends React.Component {
                             </div>
 
                         </div>
+
+                        {/* secondary reply*/}
+                        <ul className="panel-body row">
+                            <li>
+                                {/*<div className="col-md-1 reply-like">
+                                    <div className="agree pull-left text-center">
+                                        <span className="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
+                                        <p>26</p>
+                                    </div>
+                                </div>*/}
+                                <div className="col-md-11 secondary-reply">
+                                    <div className="reply-caption">
+                                        <span className="reply-name">Christian Cleber</span>&nbsp;&nbsp;
+                                        <span className="reply-profile">Mentor</span>&nbsp;&nbsp;
+                                        <span className="reply-date">a month ago</span>
+                                    </div>
+                                    <p className="reply-content">
+                                        A minimum viable product has just those core features sufficient to deploy the product,
+                                        and no more. Developers typically deploy the product to a subset of possible customersâ€”such
+                                        as early adopters thought to be more forgiving, more likely to give feedback, and able to
+                                        grasp a product vision from an early prototype or marketing information. This strategy
+                                        targets avoiding building products that customers do not want and seeks to maximize
+                                        information about the customer per dollar spent. `The minimum viable product is that
+                                        version of a new product a team uses to collect the maximum amount of validated learning
+                                        about customers with the least effort.
+                                    </p>
+                                    <div className="action-area">
+                                        <div href="#">
+
+                                            <a>
+                                                <i className="glyphicon glyphicon-thumbs-up"></i>
+                                                &nbsp;&nbsp;1
+                                                <span>&nbsp;Upvote &nbsp;&nbsp;</span>
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                {/*<div className="col-md-1 reply-like">
+                                 <div className="agree pull-left text-center">
+                                 <span className="glyphicon glyphicon-heart-empty" aria-hidden="true"></span>
+                                 <p>26</p>
+                                 </div>
+                                 </div>*/}
+                                <div className="col-md-11 secondary-reply">
+                                    <div className="reply-caption">
+                                        <span className="reply-name">Christian Cleber</span>&nbsp;&nbsp;
+                                        <span className="reply-profile">Mentor</span>&nbsp;&nbsp;
+                                        <span className="reply-date">a month ago</span>
+                                    </div>
+                                    <p className="reply-content">
+                                        A minimum viable product has just those core features sufficient to deploy the product,
+                                        and no more. Developers typically deploy the product to a subset of possible customersâ€”such
+                                        as early adopters thought to be more forgiving, more likely to give feedback, and able to
+                                        grasp a product vision from an early prototype or marketing information. This strategy
+                                        targets avoiding building products that customers do not want and seeks to maximize
+                                        information about the customer per dollar spent. `The minimum viable product is that
+                                        version of a new product a team uses to collect the maximum amount of validated learning
+                                        about customers with the least effort.
+                                    </p>
+                                    <div className="action-area">
+                                        <div href="#">
+
+                                            <a>
+                                                <i className="glyphicon glyphicon-thumbs-up"></i>
+                                                &nbsp;&nbsp;1
+                                                <span>&nbsp;Upvote &nbsp;&nbsp;</span>
+                                            </a>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </li>
+
+                        </ul>
                     </li>
 
                 </ul>
